c1df2bb31025d3c9263ec1ff4f05ebf0b5decdde
cod0216 <cod0216@nate.com>
2023-10-20 16:30:04 +0900

    fixed cmd.c basefile commit in dev branch

--- cmd.c
+++ cmd.c
@@ -13,12 +13,62 @@ int argc, optc;
 #define EQUAL(_s1, _s2) (strcmp(_s1, _s2) == 0) // 문자열이 같으면 ture
 #define NOT_EQUAL(_s1, _s2) (strcmp(_s1, _s2) != 0) //문자열이 다르면 ture
 
-static int check_arg(int count) { reutrn(0); }
-static int check_opt(char *opt) { reutrn(0); }
-
 static char *get_argv_optv(char *cmd_line) {
 	return (cmd = strtok(cmd_line, " \t\n")); 
 }
+static void print_usage(char *msg, char *cmd, char *opt, char *arg) {
+	//msg 는 "사용법 : " 글을 받음 proc_cmd() 함수 정의 보셈
+	printf("%s	%s", msg, cmd);
+	if (NOT_EQUAL(opt, ""))
+		printf("	%s", opt);
+	printf("	%s", arg);
+	printf("\n");
+
+static int check_arg(int count) {
+	if (count < 0) {
+		count = -count;
+		if(argc <= count)
+			return(0);
+	}
+	if (argc == count)
+		return 0;
+
+	if (argc > count)
+		printf("불필요한 명령어 인자가 있습니다.\n");
+	
+	else 
+		printf("명령어 인자의 수가 부족합니다.\n");
+	return -1;
+}
+
+static int check_opt(char *opt) {
+	int i, err = 0;
+	for (i = 0; i < optc; ++i) {
+		if( NOT_EQUAL(opt, optvpi)) {
+			printf("지원되지 않는 명령어 옵션(%s)입니다. \n", optv[i]);
+			err=-1;
+		}
+	}
+	return(err)l
+}
+
+void char *get_argv_optv(char *cmd_line) {
+	char *tok;
+	argc = optc = 0;
+
+	if ((cmd - strtok(cmd_line, " \t\n")) == NULL ) //첫 토큰 잘라 냄
+		return (NULL);
+
+	for ( ; (tok = strtok(NULL, " \t\n")) != NULL; ) {
+		if(tok[0] == '-')
+			optv[optc++] = tok;
+		else
+			argv[argc++] = tok;
+	}
+	return(cmd);
+}
+
+void help();
 
 void cp(void) {
 	printf("현재 이 명령어는 구현되지 않았습니다.\n"); 
@@ -61,7 +111,33 @@ cmd_tbl_t cmd_tbl [] = {
 
 int num_cmd = sizeof(cmd_tbl) / sizeof(cmd_tbl[0]); // 전체크기 vs 한칸의 크기
 
-int mait(int argc, char *argv[]) {
+void help() {
+	int k;
+	printf( "현재 지원되는 명령어 종류입니다. \n");
+	for (k = 0l k < num_cmd; ++k)
+		print_usage("	", cmd_tbl[k].cmd, cmd_tbl[k].opt, cmd_tbl[k].arg);
+	printf("\n");
+}
+
+void proc_cmd() {
+	int k;
+	for (k = 0; k < num_cmd; ++k) { // 입력한 명령어 정보를 cmd_tbl[]에서 찾음
+		//전역변수 char *cmd; 사용자가 입력한 명령어 문자열 주소. 예) cmd ->ls
+		if (EQUAL(cmd, cmd_tbl[k].cmd)) {
+			if ((check_arg(cmd_tbl[k].argc) < 0) || (check_opt(cmd_tbl[k].opt) < 0))
+			// 인자 개수 또는 옵션이 잘못된 경우임 : 사용법이 출력
+				print_usage("사용법 : " , cmd_tbl[k].cmd, cmd_tbl[k].opt, cmd_tbl[k].arg);
+				
+			else
+				cmd_tbl[k].func();
+			return;
+		}
+	}
+	printf("%s : 지원되지 않은 명령어 입니다.\n", cmd);
+}
+	
+
+int main(int argc, char *argv[]) {
 	char cmd_line[SZ_STR_BUF];
 
 	setbuf(stdout, NULL); //표준 출력 버퍼 제거: printf()즉시 화면 출력
