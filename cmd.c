f72aa26792c7d11028a245947a08f4df4545d2ad
cod0216 <cod0216@nate.com>
2023-11-22 17:08:22 +0900

    updated cmd.c file from MACOS

--- cmd.c
+++ cmd.c
@@ -1,7 +1,7 @@
-#include <stdio.h> // 입출력
-#include <string.h> // 문자열 비교
-#include <stdlib.h> // 메모리 관련 및 exit()
-#include <unistd.h> // 유닉스 시스템에 사용하는 표준 인터페이스
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
 #include <sys/stat.h>
 #include <dirent.h>
 #include <pwd.h>
@@ -23,15 +23,14 @@ char *cmd;
 char *argv[100];
 char *optv[10];
 int argc, optc;
-char cur_work_dir[SZ_STR_BUF]; // 현재 디렉토리 위치 저장용
-#define AC_LESS_1 -1 //명령어 인자 개수가 0 또는 1인 경우
-#define AC_ANY -100 //명령어 인자 개수 제한 없는 경우 (echo처럼)
-#define EQUAL(_s1, _s2) (strcmp(_s1, _s2) == 0) // 문자열이 같으면 ture
-#define NOT_EQUAL(_s1, _s2) (strcmp(_s1, _s2) != 0) //문자열이 다르면 ture
+char cur_work_dir[SZ_STR_BUF]; 
+#define AC_LESS_1 -1 
+#define AC_ANY -100 
+#define EQUAL(_s1, _s2) (strcmp(_s1, _s2) == 0) 
+#define NOT_EQUAL(_s1, _s2) (strcmp(_s1, _s2) != 0) 
 #define PRINT_ERR_RET() do{ perror(cmd); return; }while(0)
 
 static void print_usage(char *msg, char *cmd, char *opt, char *arg) {
-	//msg 는 "사용법 : " 글을 받음 proc_cmd() 함수 정의 보셈
 	printf("%s	%s", msg, cmd);
 	if (NOT_EQUAL(opt, ""))
 		printf("	%s", opt);
@@ -73,7 +72,7 @@ static char *get_argv_optv(char *cmd_line) {
 	char *tok;
 	argc = optc = 0;
 
-	if ((cmd = strtok(cmd_line, " \t\n")) == NULL ) //첫 토큰 잘라 냄
+	if ((cmd = strtok(cmd_line, " \t\n")) == NULL ) 
 		return (NULL);
 
 	for ( ; (tok = strtok(NULL, " \t\n")) != NULL; ) {
@@ -332,7 +331,16 @@ if (rmdir(argv[0]) < 0)
 }
 
 void quit(void) {
-	exit(0);// exit(0)함수는 unix가 제공해준다
+	exit(0);
+}
+
+void Sleep() {
+	int sec;
+	if(sscanf(argv[0], "%d", &sec) < 1)
+		PRINT_ERR_RET();
+	else
+		sleep(sec);
+
 }
 
 void makedir() {
@@ -387,11 +395,11 @@ void unixname() {
 }	
 
 typedef struct {
-	char *cmd;					//명령어 문자열 시작 주소
-	void (*func)(void);	//명령어 처리하는 함수 포인터(함수 이름, 명령어 주소)
-	int argc;						//명령어 인자의 개수(명령어와 옵션은 제외)
-	char *opt;					//옵션 문자열 시작 주소: 예) "-a",옵션 없으면 ""
-	char *arg;					//명령어 사용법 출력할 떄 사용할 명령어 인자 문자열 시작 주소
+	char *cmd;				
+	void (*func)(void);	
+	int argc;						
+	char *opt;				
+	char *arg;					
 } cmd_tbl_t;
 
 cmd_tbl_t cmd_tbl [] = {
@@ -408,6 +416,7 @@ cmd_tbl_t cmd_tbl [] = {
 	{ "pwd",	pwd,	0,	"",	"" },
 	{ "exit", quit, 0, "", "" },
 	{ "rm",	rm,	1,	"", "파일이름" },
+	{ "sleep",	Sleep,	1,	"", "초단위시간" },
 	{ "mkdir",	makedir,	1,	"", "디렉토리이름" },
 	{ "mv", mv, 2, "", "원본파일 바뀐이름" },
 	{ "touch",	touch,	1,	"", "파일이름" },
@@ -416,7 +425,7 @@ cmd_tbl_t cmd_tbl [] = {
 	{ "rmdir", removedir, 1, "", "디렉토리이름"},	
 };
 
-int num_cmd = sizeof(cmd_tbl) / sizeof(cmd_tbl[0]); // 전체크기 vs 한칸의 크기
+int num_cmd = sizeof(cmd_tbl) / sizeof(cmd_tbl[0]); 
 
 void help() {
 	int k;
@@ -454,11 +463,9 @@ void run_cmd() {
 
 void proc_cmd() {
 	int k;
-	for (k = 0; k < num_cmd; ++k) { // 입력한 명령어 정보를 cmd_tbl[]에서 찾음
-		//전역변수 char *cmd; 사용자가 입력한 명령어 문자열 주소. 예) cmd ->ls
+	for (k = 0; k < num_cmd; ++k) { 
 		if (EQUAL(cmd, cmd_tbl[k].cmd)) {
 			if ((check_arg(cmd_tbl[k].argc) < 0) || (check_opt(cmd_tbl[k].opt) < 0))
-			// 인자 개수 또는 옵션이 잘못된 경우임 : 사용법이 출력
 				print_usage("사용법 : " , cmd_tbl[k].cmd, cmd_tbl[k].opt, cmd_tbl[k].arg);
 				
 			else
@@ -473,16 +480,16 @@ int main(int argc, char *argv[]) {
 	int cmd_count = 1;
 	char cmd_line[SZ_STR_BUF];
 	
-	setbuf(stdout, NULL); //표준 출력 버퍼 제거: printf()즉시 화면 출력
-	setbuf(stderr, NULL); //표준 에러 출력 버퍼 제거
+	setbuf(stdout, NULL); 
+	setbuf(stderr, NULL); 
 	
 	help();
 	getcwd(cur_work_dir, SZ_STR_BUF);
 	
 	for( ; ; ) {
 		printf("<%s> %d : ", cur_work_dir, cmd_count);
-		fgets(cmd_line, SZ_STR_BUF, stdin); // 키보드에서 한 행 전체를 입력 받아 cmd_line에 저장
-		if ( get_argv_optv(cmd_line) !=NULL) {// 입력받은 문자를 개별 문자열로 분리
+		fgets(cmd_line, SZ_STR_BUF, stdin); 
+		if ( get_argv_optv(cmd_line) !=NULL) {
 			proc_cmd();
 			cmd_count++;
 		}
